b8b2f196ae5fa611a645008fa0a4fd36562a11de
Dishant radadiya <dishantradadiya3036@gmail.com>
2024-07-07 08:50:50 +0530

    Shutting the day even before starting

---
+++ Practice-Questions/Leetcode/1518.Water-Bottles.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    
+private: 
+    
+    void solve( int& ans, int e, int Empty, int filled){
+        
+        if( filled == 0 && Empty < e)
+            return;
+        
+        ans += filled;
+        Empty += filled;
+        filled = 0;
+        
+        while(Empty >= e){
+            
+            Empty -= e;
+            filled++;
+            
+        }
+            
+        solve( ans, e, Empty, filled);
+        
+    }
+    
+    
+public:
+    
+    int numWaterBottles(int numBottles, int numExchange) {
+        
+        int empty = numBottles;
+        int filled = 0;
+        int ans = numBottles;
+        
+        solve(ans, numExchange, empty, filled);
+        
+        return ans;
+        
+    }
+    
+};
