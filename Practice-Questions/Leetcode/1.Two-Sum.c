2d04d233878fe665f10e0354dd9ee97dc4caee92
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-01-02 18:21:10 +0530

    Minor Changes

--- Practice-Questions/Leetcode/1.Two-Sum.c
+++ Practice-Questions/Leetcode/1.Two-Sum.c
@@ -2,7 +2,7 @@
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     
-    int *ans = (int*) malloc( sizeof(int) * 2 );
+int *ans = (int*) malloc( sizeof(int) * 2 );
     
     for(int i = 0 ;  i<numsSize ; i++){
         
