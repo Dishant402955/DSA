926aef42537c7b6f3931a2204df1617ba99d650b
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2025-07-11 16:46:59 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/2402.Meeting-Rooms-III.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        priority_queue<long long, vector<long long>, greater<long long>> freeRooms;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> busyRooms;
+        vector<int> count(n, 0);
+        for (int i = 0; i < n; ++i) freeRooms.push(i);
+        for (auto& m : meetings) {
+            int start = m[0], end = m[1];
+            while (!busyRooms.empty() && busyRooms.top().first <= start) {
+                freeRooms.push(busyRooms.top().second);
+                busyRooms.pop();
+            }
+            if (!freeRooms.empty()) {
+                int room = freeRooms.top(); freeRooms.pop();
+                count[room]++;
+                busyRooms.push({end, room});
+            } else {
+                auto [availTime, room] = busyRooms.top(); busyRooms.pop();
+                count[room]++;
+                busyRooms.push({availTime + (end - start), room});
+            }
+        }
+        int maxRoom = 0;
+        for (int i = 1; i < n; ++i)
+            if (count[i] > count[maxRoom]) maxRoom = i;
+        return maxRoom;
+    }
+};
