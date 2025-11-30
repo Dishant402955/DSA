4e102bb9a9fdcdd1279be5cf3bd9e2f7e38474de
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2025-11-30 11:24:48 +0530

    DL STRK

---
+++ 1590.Make-Sum-Divisible-by-P.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+         long long total = 0;
+        for (int x : nums) total += x;
+
+        int need = total % p;
+        if (need == 0) return 0; 
+
+        unordered_map<int, int> mp;
+        mp[0] = -1; 
+
+        long long prefix = 0;
+        int ans = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix = (prefix + nums[i]) % p;
+
+            int target = (prefix - need + p) % p;
+
+            if (mp.count(target)) {
+                ans = min(ans, i - mp[target]);
+            }
+
+            mp[prefix] = i;
+        }
+
+        return (ans >= nums.size() ? -1 : ans);
+    }
+};
