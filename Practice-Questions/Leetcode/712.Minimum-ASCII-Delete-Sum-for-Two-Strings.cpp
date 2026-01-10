55ca66c7c9233c02ffa6f0766ce89ba3c4a8a5f4
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-01-10 08:57:09 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/712.Minimum-ASCII-Delete-Sum-for-Two-Strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = s1[i] + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        int total = 0;
+        for (char c : s1) total += c;
+        for (char c : s2) total += c;
+
+        return total - 2 * dp[0][0];
+    }
+};
