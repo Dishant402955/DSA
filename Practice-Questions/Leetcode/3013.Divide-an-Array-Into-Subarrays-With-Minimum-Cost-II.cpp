40f847a513bbbaffee04055df3d30bef91445819
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-02-02 08:08:45 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/3013.Divide-an-Array-Into-Subarrays-With-Minimum-Cost-II.cpp
@@ -0,0 +1,69 @@
+//pmr multiset version
+#include <memory_resource>
+class Solution {
+    long long Sum=0;
+    std::pmr::unsynchronized_pool_resource pool;
+    //Define the multisets using the PMR namespace
+    std::pmr::multiset<int> small{&pool};
+    std::pmr::multiset<int> large{&pool};
+
+    void add(int x, int m) {
+        small.insert(x);
+        Sum+=x;
+        if (small.size()>m) {// small should have at most m elements
+            int toMove=*small.rbegin();
+            Sum-=toMove;
+            large.insert(toMove);
+            small.erase(prev(small.end()));
+        }
+    }
+
+    void remove(int x) {
+        auto it=small.find(x);
+        if (it!=small.end()) {
+            Sum-=x;
+            small.erase(it);
+            if (!large.empty()) {
+                auto lit=large.begin(); // Use iterator 
+                int toMove=*lit;
+                Sum+=toMove;
+                small.insert(toMove);
+                large.erase(lit); // removal by iterator
+            }
+        } 
+        else {
+            auto itL=large.find(x);
+            large.erase(itL); //remove
+        }
+    }
+
+public:
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        const int n=nums.size();
+        k--; // Need k-1 more elements after nums[0]
+
+        // Initialize the first window [1, dist+1]
+        const int iN=dist+2;
+        vector<int> win(nums.begin()+1, nums.begin()+iN);
+    
+        // initialize small & large in linear time
+        // the (k-1)-th smallest element at its sorted position
+        nth_element(win.begin(), win.begin()+k, win.end());// O(dist+2)
+        small.insert(win.begin(), win.begin() + k);
+        Sum=accumulate(win.begin(), win.begin()+k, 0LL);
+        large.insert(win.begin()+k, win.end());
+    
+        long long minCost=nums[0]+Sum;
+
+        // Slide the window starting from index 1.
+        // Elements in window nums[l...r]
+        for (int l=1, r=dist+2; r<n; l++, r++) {
+            remove(nums[l]);
+            add(nums[r], k);
+
+            // Update the minCost
+            minCost=min(minCost, nums[0]+Sum);
+        }
+        return minCost;
+    }
+};
