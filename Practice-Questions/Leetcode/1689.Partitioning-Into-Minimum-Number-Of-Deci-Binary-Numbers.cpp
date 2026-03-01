9325e00191ad829d3edf4f269bfe478f3986ae7f
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-03-01 10:21:51 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/1689.Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int ans = 0;
+
+        for(int i=0;i<n.length();i++){
+            if(n[i]-'0' > ans) ans = n[i] -'0';
+        }
+        return ans;
+    }
+};
