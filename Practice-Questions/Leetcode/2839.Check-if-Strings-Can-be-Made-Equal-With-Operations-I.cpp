0b82bef2d1060fbd57200df7aed85e4472c55677
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-03-29 08:14:58 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/2839.Check-if-Strings-Can-be-Made-Equal-With-Operations-I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        // check even positions
+        bool even = (s1[0] == s2[0] && s1[2] == s2[2]) ||
+                    (s1[0] == s2[2] && s1[2] == s2[0]);
+
+        // check odd positions
+        bool odd = (s1[1] == s2[1] && s1[3] == s2[3]) ||
+                (s1[1] == s2[3] && s1[3] == s2[1]);
+
+        return even && odd;
+    }
+};
