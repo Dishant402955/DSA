96f489a12787ba458b34f85f3ba5a501bef78dab
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-04-06 09:21:38 +0530

    DL STRK

--- Practice-Questions/Leetcode/874.Walking-Robot-Simulation.cpp
+++ Practice-Questions/Leetcode/874.Walking-Robot-Simulation.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
-    static int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_map<int, set<int>> XobSet, YobSet;
-        XobSet.reserve(10000), YobSet.reserve(10000);
-        for(auto& ob: obstacles){
-            int x=ob[0], y=ob[1];
-            XobSet[x].insert(y);
-            YobSet[y].insert(x);
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        // Store obstacles for quick lookup
+        set<pair<int,int>> blocked;
+        for (auto &o : obstacles) {
+            blocked.insert({o[0], o[1]});
         }
 
-        const int dir[4][2]={{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-        int x=0, y=0, dx=0, dy=1, face=0, maxD2=0;
-        for(int c: commands){
-            switch(c){
-                case -2: face=(face+1)%4; dx=dir[face][0]; dy=dir[face][1]; break;
-                case -1: face=(face+3)%4; dx=dir[face][0]; dy=dir[face][1]; break;
-                default:
-                //    cout<<"\nc="<<c<<" face="<<face<<":";
-                    switch(face){
-                        case 0: {//up
-                            auto it=upper_bound(XobSet[x].begin(), XobSet[x].end(), y);
-                            if (it!=XobSet[x].end() && *it<=y+c) y=*it-1;
-                            else y+=c;
-                            break;
-                        }
-                        case 1: {//left
-                            auto it=upper_bound(YobSet[y].rbegin(), YobSet[y].rend(), x, greater<>());
-                            if (it!=YobSet[y].rend() && *it>=x-c) x=*it+1;
-                            else x-=c;
-                            break;
-                        }
-                        case 2: {//down
-                            auto it=upper_bound(XobSet[x].rbegin(), XobSet[x].rend(), y, greater<>());
-                            if (it!=XobSet[x].rend() && *it>=y-c) y=*it+1;
-                            else y-=c;
-                            break;
+        // Directions: North, East, South, West
+        vector<pair<int,int>> directions = {
+            {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+        };
+
+        int x = 0, y = 0;
+        int dir = 0; // start facing North
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                // turn right
+                dir = (dir + 1) % 4;
+            } 
+            else if (cmd == -2) {
+                // turn left
+                dir = (dir + 3) % 4;
+            } 
+            else {
+                // move forward step by step
+                while (cmd--) {
+                    int nx = x + directions[dir].first;
+                    int ny = y + directions[dir].second;
+
+                    // stop if obstacle is ahead
+                    if (blocked.count({nx, ny})) break;
+
+                    x = nx;
+                    y = ny;
+
+                    maxDist = max(maxDist, x * x + y * y);
+                }
+            }
+        }
+
+        return maxDist;
+    }
+};                            break;
                         }
                         case 3: {//right
                             auto it=upper_bound(YobSet[y].begin(), YobSet[y].end(), x);
