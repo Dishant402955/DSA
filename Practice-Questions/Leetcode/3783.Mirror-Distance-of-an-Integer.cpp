0e6a56d8b0adfda2b8151429f317220cd5bbb8ac
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-04-18 11:49:57 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/3783.Mirror-Distance-of-an-Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int copy = n;
+        int rev = 0;
+        while(n>0){
+            rev = rev*10 + n%10;
+            n/=10;
+        }
+        return abs(rev-copy);
+    }
+};
+
+int main(){
+    Solution s;
+    int n = 23;
+    cout<<s.mirrorDistance(n)<<endl;
+}
