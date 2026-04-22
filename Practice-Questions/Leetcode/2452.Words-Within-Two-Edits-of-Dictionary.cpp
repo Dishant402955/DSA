c709f323052606643480db3407ddc6024688b9c7
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-04-22 09:08:57 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/2452.Words-Within-Two-Edits-of-Dictionary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> ans;
+
+        for (string q : queries) {
+            for (string d : dictionary) {
+                int diff = 0;
+
+                for (int i = 0; i < q.size(); i++) {
+                    if (q[i] != d[i]) diff++;
+
+                    if (diff > 2) break;
+                }
+
+                if (diff <= 2) {
+                    ans.push_back(q);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
