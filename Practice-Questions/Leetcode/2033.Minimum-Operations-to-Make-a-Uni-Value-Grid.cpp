5be21e0ac8ad0c3c19e2b5daf123daf900e99066
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-04-28 08:22:39 +0530

    DL STRK

--- Practice-Questions/Leetcode/2033.Minimum-Operations-to-Make-a-Uni-Value-Grid.cpp
+++ Practice-Questions/Leetcode/2033.Minimum-Operations-to-Make-a-Uni-Value-Grid.cpp
@@ -1,22 +1,25 @@
+
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int> arr;
-        for (auto& row : grid) {
-            for (int num : row) {
-                arr.push_back(num);
+
+        for (auto &row : grid) {
+            for (int val : row) {
+                arr.push_back(val);
             }
         }
-        
+
         sort(arr.begin(), arr.end());
         int median = arr[arr.size() / 2];
-        int operations = 0;
 
+        int operations = 0;
         for (int num : arr) {
             int diff = abs(num - median);
             if (diff % x != 0) return -1;
             operations += diff / x;
         }
+
         return operations;
     }
 };
