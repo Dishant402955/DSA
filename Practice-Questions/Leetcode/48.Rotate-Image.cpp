6ec4fddc7001f1a4f944dfb6aff0c70bb76d3296
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-05-04 22:12:20 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/48.Rotate-Image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // Transpose
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // Reverse each row
+        for(int k = 0; k < n; k++){
+            int i = 0, j = n - 1;
+            while(i < j) { 
+                swap(matrix[k][i], matrix[k][j]);
+                i++;
+                j--;
+            }
+        }
+    }
+};
