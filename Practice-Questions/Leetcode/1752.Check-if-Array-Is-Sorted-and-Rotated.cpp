e88c8f46ea6b1a9ca221d5c4dd193ef9da1b2f0b
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-05-23 16:34:01 +0530

    DL STRK

--- Practice-Questions/Leetcode/1752.Check-if-Array-Is-Sorted-and-Rotated.cpp
+++ Practice-Questions/Leetcode/1752.Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -1,12 +1,13 @@
 class Solution {
- public:
-  bool check(vector<int>& nums) {
-    const int n = nums.size();
-    int rotates = 0;
-    for (int i = 0; i < n; ++i){
-      if (nums[i] > nums[(i + 1) % n] && ++rotates > 1)
-        return false;
+public:
+    bool check(vector<int>& nums) {
+        bool fault = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+            if (nums[i] > nums[(i + 1) % n] && exchange(fault, 1))
+                return 0;
+
+        return 1;
     }
-    return true;
-  }
 };
