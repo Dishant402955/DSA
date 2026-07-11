777a9faaa8ffec626a1e23b6758438c586c66b78
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-07-11 21:48:50 +0530

    DL STRK

--- Practice-Questions/Leetcode/2685.Count-the-Number-of-Complete-Components.cpp
+++ Practice-Questions/Leetcode/2685.Count-the-Number-of-Complete-Components.cpp
@@ -1,50 +1,30 @@
 class Solution {
-private:
-    bool travelBFS(int src, vector<bool>& vis, vector<vector<int>>& adj){
-        int nodes = 0;
-        int edges = 0;
-
-        queue<int> q;
-        q.push(src);
-        vis[src] = 1;
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            nodes++;
-
-            for(auto adjN:adj[node]){
-                edges++;
-                if(!vis[adjN]){
-                    q.push(adjN);
-                    vis[adjN] = 1;
-                }
-            }
-        }
-
-        return nodes*(nodes-1)==edges;
-    }
-
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<bool> vis(n,0);
-        vector<vector<int>> adj(n);
-
-        for(auto edge:edges){
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis, int& nodes, int& degSum) {
+        vis[node]=1;
+        nodes++;
+        degSum+=adj[node].size();
+        for(int nei:adj[node]){
+            if(!vis[nei])
+                dfs(nei,adj,vis,nodes,degSum);
         }
-
-        int cnt = 0;
-
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for(auto &e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        vector<int> vis(n,0);
+        int ans=0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                if(travelBFS(i,vis,adj)){
-                    cnt++;
-                }
+                int nodes=0,degSum=0;
+                dfs(i,adj,vis,nodes,degSum);
+                int edgeCount=degSum/2;
+                if(edgeCount==nodes*(nodes-1)/2) ans++;
             }
         }
-
-        return cnt;
+        return ans;
     }
 };
