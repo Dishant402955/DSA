e1488529419ab798908171e77edc146fcd85d99d
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-07-23 08:21:00 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/3513.Number-of-Unique-XOR-Triplets-I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n <= 2) return n;
+
+        int bits = 0;
+        int x = n;
+        while (x) {
+            bits++;
+            x >>= 1;
+        }
+
+        return 1 << bits;
+    }
+};
