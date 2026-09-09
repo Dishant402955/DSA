c0d0a95f379eee0b8295fde70dad415d89f918d6
Dishant radadiya <164029200+Dishant402955@users.noreply.github.com>
2026-09-09 09:48:13 +0530

    DL STRK

---
+++ Practice-Questions/Leetcode/3871.Count-Commas-in-Range-II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if (n < 1000)
+            return 0;
+
+        long long ans = n - 999;
+
+        if (n >= 1000000)
+            ans += n - 999999;
+
+        if (n >= 1000000000)
+            ans += n - 999999999;
+
+        if (n >= 1000000000000)
+            ans += n - 999999999999;
+
+        if (n >= 1000000000000000)
+            ans += n - 999999999999999;
+
+        return ans;
+    }
+};
